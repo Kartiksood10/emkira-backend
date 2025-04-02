5b6ee507db884cd7ecaa2a5633997e2cd4098af9
Kartiksood10 <kartiksood2002@gmail.com>
2025-04-02 14:48:07 +0530

    Swagger UI Epic controller

--- src/main/java/com/project/emkira/controller/EpicController.java
+++ src/main/java/com/project/emkira/controller/EpicController.java
@@ -4,6 +4,9 @@ import com.project.emkira.dto.EpicRequest;
 import com.project.emkira.dto.EpicStatusRequest;
 import com.project.emkira.model.Epic;
 import com.project.emkira.service.EpicService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +14,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/epics")
+@Tag(name = "Epic Controller", description = "APIs for managing epics")
+@SecurityRequirement(name = "BearerAuth")
 public class EpicController {
 
     private final EpicService epicService;
@@ -22,36 +27,42 @@ public class EpicController {
     }
 
     @PostMapping("/add")
+    @Operation(summary = "Add a new epic", description = "Adds a new epic")
     public Epic addEpic(@RequestBody EpicRequest epicRequest) {
 
         return epicService.addEpic(epicRequest);
     }
 
     @GetMapping("/projects/{projectId}")
+    @Operation(summary = "Get epic by ID", description = "Retrieves List of epics by project")
     public List<Epic> getProjectById(@PathVariable Long projectId) {
 
         return epicService.getProjectById(projectId);
     }
 
     @PutMapping("/status/{epicId}")
+    @Operation(summary = "Update epic status", description = "Updates the status of epic (new, todo etc.)")
     public Epic updateEpicStatus(@PathVariable Long epicId, @RequestBody EpicStatusRequest request) {
 
         return epicService.updateEpicStatus(epicId, request);
     }
 
     @DeleteMapping("/delete/{epicId}")
+    @Operation(summary = "Delete an epic by ID", description = "Deletion of epic")
     public String deleteEpic(@PathVariable Long epicId) {
 
         return epicService.deleteEpic(epicId);
     }
 
     @GetMapping("/priority/{priority}")
+    @Operation(summary = "Get epic by priority", description = "Retrieve epic by priority")
     public List<Epic> getEpicByPriority(@PathVariable Epic.Priority priority) {
 
         return epicService.getEpicByPriority(priority);
     }
 
     @GetMapping("/status/{status}")
+    @Operation(summary = "Get epic by status", description = "Retrieve epic by status")
     public List<Epic> getEpicByStatus(@PathVariable Epic.Status status) {
 
         return epicService.getEpicByStatus(status);
