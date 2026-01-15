a334fd10829355ad5491cc5cdfcf4512f807fe64
Kartiksood10 <kartiksood2002@gmail.com>
2026-01-15 18:19:53 +0530

    Testcontainers for jenkins

--- src/test/java/com/project/emkira/EmkiraApplicationTests.java
+++ src/test/java/com/project/emkira/EmkiraApplicationTests.java
@@ -2,12 +2,24 @@ package com.project.emkira;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Testcontainers
 class EmkiraApplicationTests {
+
+	@Container
+	static PostgreSQLContainer<?> postgres =
+			new PostgreSQLContainer<>("postgres:15-alpine")
+					.withDatabaseName("testdb")
+					.withUsername("test")
+					.withPassword("test");
 
 	@Test
 	void contextLoads() {
 	}
-
 }
